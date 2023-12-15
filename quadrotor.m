c02eb65795ea6f52ea2a108fb9cf7113c37b7e68
ldjennings <ldjennings@github.com>
2023-12-15 05:11:28 -0500

    can complete full sequence

--- quadrotor.m
+++ quadrotor.m
@@ -32,10 +32,10 @@ end
 captured = captured || isCaptured(UAV_Trajectory(t), z);
 
 if stabilized
-    fprintf("Hi mom!\n")
+    u = return_controller(z,p);
 elseif captured
     u = stabilize_controller(z,p);
-    stabilized = stabilized || all(abs(z(4:12) - zeros(9,1)) < .001);
+    stabilized = stabilized || all(abs(z(4:12) - zeros(9,1)) < .01);
 else
     u = intercept_controller(t, z,p);
 end
@@ -140,82 +140,38 @@ function u = stabilize_controller(Z, p)
     u = smc_controller(Z,p,dkine,kg, smc_g);    
 end
 
-%     function u = return_controller(t, Z, p)
-%     %% Extracting Parameters
-%     g = p(1); l = p(2); m = p(3); I11 = p(4); I22 = p(5); I33 = p(6); sigma = p(8);
-% 
-%     x = 0; dx = Z(7);
-%     y = 0; dy = Z(8);
-%     z = 0; dz = Z(9);
-% 
-%     phi = Z(4); theta = Z(5); psi = Z(6);
-%     [dphi, dtheta, dpsi] = calcAngV(Z(10:12), Z(4:6));
-% 
-%     xd = x; dxd =  0; ddxd = 0;
-%     yd = y; dyd =  0; ddyd = 0;
-%     zd = z; dzd =  0; ddzd = 0;
-% 
-%     %% Defining Controller Gains
-% 
-%     z_lambda = .8; z_K = 6; z_n = 1;
-%     kp = .3;
-%     kd = 1;
-% 
-%     phi_lambda   = 2;          phi_K   =  8;     phi_n   = 1;
-%     theta_lambda = phi_lambda; theta_K =  phi_K; theta_n = phi_n;
-%     psi_lambda   = 1; psi_K   = 5; psi_n   = 1.5;
-% 
-%     %% Linear Position Controls
-%     sz = (dzd - dz) + z_lambda*(zd - z);
-% 
-%     U1 = ((ddzd + g + z_lambda*(zd-z)) + z_K*sz/(abs(sz) + z_n))*m/(cos(phi)*cos(theta));
-% 
-%     Emax = 1;
-%     Fx = m * (ddxd + kd * (dxd - dx) + kp * sat(xd - x,-Emax,Emax));
-% 	Fy = m * (ddyd + kd * (dyd - dy) + kp * sat(yd - y,-Emax,Emax));
-% 
-% 
-%     %% Orientation Controls
-%     degMax = 35 * pi/180;
-% 
-%     phid = asin(sat(-Fy / U1,-degMax,degMax));
-%     thetad = asin(sat( Fx / U1,-degMax,degMax));
-%     psid = 0;
-% 
-%     ephi   = phid   - phi;
-%     etheta = thetad - theta;
-%     epsi   = psid   - psi;
-% 
-% 
-%     % Phi controller 
-%     sphi = (0 - dphi) + phi_lambda*ephi;
-%     U2 = (0 - dtheta*dpsi*(I22-I33)/I11) + phi_lambda*(0 - dphi) + ...
-%      phi_K*sphi/(abs(sphi) + phi_n);
-% 
-%     % Theta controller 
-%     stheta = (0 - dtheta) + theta_lambda*etheta;
-%     U3 = (0 + dphi*dpsi*(I11-I33)/I22) + theta_lambda*(0 - dtheta) + ...
-%          theta_K*stheta/(abs(stheta) + theta_n);
-% 
-%     % Psi controller
-%     spsi = (0 - dpsi) + psi_lambda*epsi;
-%     U4 = (0 - dphi*dtheta*(I11-I22)/I33) + psi_lambda*(0 - dpsi) + ...
-%         + psi_K*spsi/(abs(spsi) + psi_n);
-% 
-%     U = [U1 U2 U3 U4]';
-% 
-% 
-%     %% Calculating individual control inputs
-%     length_11 = l/I11;
-%     length_22 = l/I22;
-%     sigma_33 = sigma/I33;
-%     A = [         1         1         1          1;
-%                   0 length_11         0 -length_11;
-%          -length_22         0 length_22          0;
-%            sigma_33 -sigma_33  sigma_33 -sigma_33;];
-% 
-%     u = A\U;
-% end
+    function u = return_controller(Z, p)
+
+
+    xd = 0; dxd =  0; ddxd = 0;
+    yd = 0; dyd =  0; ddyd = 0;
+    zd = 0; dzd =  0; ddzd = 0;
+
+
+    z_lambda = .8; z_K = 6; z_n = 1;
+    kp = .3;
+    kd = 1;
+
+    phi_lambda   = 2;          phi_K   =  8;     phi_n   = 1;
+    theta_lambda = phi_lambda; theta_K =  phi_K; theta_n = phi_n;
+    psi_lambda   = 1; psi_K   = 5; psi_n   = 1.5;
+
+
+    dkine = [xd dxd ddxd;
+             yd dyd ddyd;
+             zd dzd ddzd;];
+    
+    kg = [kp kd];
+
+    smc_g = [z_lambda z_K, z_n;
+             phi_lambda phi_K phi_n;
+             theta_lambda theta_K theta_n;
+             psi_lambda psi_K psi_n;];
+
+
+    u = smc_controller(Z,p,dkine,kg, smc_g);  
+
+end
 
 
 function [p, v, a] = predictNextState(Prev3)
