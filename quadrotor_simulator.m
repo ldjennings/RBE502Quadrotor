e9cf298d04661f86f1f16a0455b9740dcb1f2997
ldjennings <ldjennings@github.com>
2023-12-15 22:50:14 -0500

    final adjustments

--- quadrotor_simulator.m
+++ quadrotor_simulator.m
@@ -33,39 +33,39 @@ t = linspace(0, t2, t2/dt);
 
 %% Plotting the results
 
-
+fsize = 18;
 
 for i=1:4
     ax(i) = subplot(2,2,i,'NextPlot','Add','Box','on','XGrid','on','YGrid','on',...
                 'Xlim',[t(1), t(end)],...
-                'TickLabelInterpreter','LaTeX','FontSize',14);
-    xlabel('t','Interpreter','LaTeX','FontSize',14);        
+                'TickLabelInterpreter','LaTeX','FontSize',fsize);
+    xlabel('t','Interpreter','LaTeX','FontSize',fsize);        
 end
 
 
 plot(ax(1), t,z(:,1:3), 'LineWidth', 1.5);
 legend(ax(1), {'$x_1$', '$x_2$', '$x_3$'},... 
-    'Interpreter', 'LaTeX', 'FontSize', 14);
-title(ax(1), '${\bf x}$','Interpreter','LaTeX','FontSize',14);
-xlabel(ax(1), 't','Interpreter','LaTeX','FontSize',14);
+    'Interpreter', 'LaTeX', 'FontSize', fsize);
+title(ax(1), '${\bf x}$','Interpreter','LaTeX','FontSize',fsize);
+xlabel(ax(1), 't','Interpreter','LaTeX','FontSize',fsize);
 grid on;
 
 plot(ax(3), t, z(:,4:6), 'LineWidth', 1.5);
 legend(ax(3), {'$\phi$', '$\theta$', '$\psi$'},...
-    'Interpreter', 'LaTeX', 'FontSize', 14);
-title(ax(3), '$\mathbf\alpha$','Interpreter','LaTeX','FontSize',14);
+    'Interpreter', 'LaTeX', 'FontSize', fsize);
+title(ax(3), '$\mathbf\alpha$','Interpreter','LaTeX','FontSize',fsize);
 grid on;
 
 plot(ax(2), t, z(:,7:9), 'LineWidth', 1.5);
 legend(ax(2), {'$\dot{x}_1$', '$\dot{x}_2$', '$\dot{x}_3$'},...
-    'Interpreter', 'LaTeX', 'FontSize', 14);
-title(ax(2), '$\dot{\bf x}$','Interpreter','LaTeX','FontSize',14);
+    'Interpreter', 'LaTeX', 'FontSize', fsize);
+title(ax(2), '$\dot{\bf x}$','Interpreter','LaTeX','FontSize',fsize);
 grid on;
 
 plot(ax(4), t, z(:,10:12), 'LineWidth', 1.5);
 legend(ax(4), {'$\omega_1$', '$\omega_2$', '$\omega_3$'},...
-    'Interpreter', 'LaTeX', 'FontSize', 14);
-title(ax(4), '$\mathbf\omega$','Interpreter','LaTeX','FontSize',14);
+    'Interpreter', 'LaTeX', 'FontSize', fsize);
+title(ax(4), '$\mathbf\omega$','Interpreter','LaTeX','FontSize',fsize);
 grid on;
 
 
